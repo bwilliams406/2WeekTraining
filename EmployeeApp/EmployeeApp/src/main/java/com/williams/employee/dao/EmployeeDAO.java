67dd77c0eb3d27b3c9a204c7fea67557662679f6
Williams <brodwilliams@teksystems.com>
2019-04-08 14:33:22 -0500

    Update

--- EmployeeApp/EmployeeApp/src/main/java/com/williams/employee/dao/EmployeeDAO.java
+++
@@ -1,9 +0,0 @@
-package com.williams.employee.dao;
-
-import java.util.List;
-
-import com.williams.employee.model.EmployeeModel;
-
-public interface EmployeeDAO {
-	public List<EmployeeModel> getAllEmployees();
-}
--- EmployeeApp/EmployeeApp/src/main/java/com/williams/employee/service/EmployeeService.java
+++
@@ -1,9 +0,0 @@
-package com.williams.employee.service;
-
-import java.util.List;
-
-import com.williams.employee.model.EmployeeModel;
-
-public interface EmployeeService {
-	public List<EmployeeModel> getAllEmployees();
-}
